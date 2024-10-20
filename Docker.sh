a07c939ef81ae87d6509f2633c54b9e8ffaca478
viniciius14 <rodri.f.ribeiro@gmail.com>
2024-10-20 16:41:57 +0100

    .

--- Docker.sh
+++ Docker.sh
@@ -6,3 +6,13 @@ sudo docker run hello-world
 apt-get install x11-xserver-utils
 xhost +local:docker
 
+# Make the Image
+docker build .
+
+# Run the container
+docker run -it \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --device=/dev/kvm \
+    --privileged \
+    GeckOS_Container
