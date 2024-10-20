c6770aca55c45a9225c2c5e99c74bbd69e174362
viniciius14 <rodri.f.ribeiro@gmail.com>
2024-10-20 17:12:39 +0100

    .

--- Docker.sh
+++ Docker.sh
@@ -1,11 +1,11 @@
 # Requirements
-sudo apt install docker.io
-sudo systemctl start docker
-apt-get install x11-xserver-utils
-xhost +local:docker
+sudo apt install docker.io | tee log.txt
+sudo systemctl start docker | tee -a log.txt
+apt-get install x11-xserver-utils | tee -a log.txt
+xhost +local:docker | tee -a log.txt
 
 # Make the Image
-sudo docker build misc/docker/. --tag geckos_container
+sudo docker build misc/docker/. --tag geckos_container | tee -a log.txt
 
 # Run the container
 sudo docker run -it \
@@ -13,4 +13,4 @@ sudo docker run -it \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --device=/dev/kvm \
     --privileged \
-    geckos_container
+    geckos_container | tee -a log.txt
